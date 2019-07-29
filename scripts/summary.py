29346d4ae90c7d090a4d235a4d5922c9909e278d
Fabian Neumann <fabian.neumann@outlook.de>
2019-07-29 21:18:01 +0200

    summary: fix dtypes

--- scripts/summary.py
+++ scripts/summary.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import numpy as np
 
-concat_list = []
+df = None
+
 for scenario in snakemake.input:
     series = pd.read_csv(scenario, header=None, index_col=0).squeeze()
-    concat_list.append(series)
+    series.name = scenario[18:-4]
+    if df is None:
+        df = pd.DataFrame(series)
+    else:
+        df = df.join(series)
 
-df = pd.condat(concat_list, axis=1)
+df = df.T
+
+dtypelist = [np.int, np.int, np.int, np.float, 
+          np.int, np.int, np.int, np.int,
+          np.int, np.int ,np.int ,np.int,
+          np.int, np.str, np.float, np.float,
+          np.float, np.float, np.float,
+          np.float, np.float, np.float,
+          np.float, np.int, np.int, np.int,
+          np.float, np.str, np.float, np.int,
+          np.int, np.int, np.float, np.float,
+          np.float, np.float, np.float,
+          np.float, np.float, np.float]
+dtypes = dict(zip(df.columns, dtypelist))
+df = df.astype(dtypes)
 
 df.to_csv(snakemake.output[0])
